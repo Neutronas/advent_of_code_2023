784a72a8484429c1dc3b670cb8e1e6acbc4947bb
Lukas Ružauskas <nequadora@gmail.com>
2023-12-13 12:48:19 +0200

    Part 2 for day12. Using memoization and cache

--- 12/main_part2.py
+++ 12/main_part2.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+import functools
 
 def read_input(file_path):
     with open(file_path, 'r') as file:
@@ -7,7 +7,7 @@ def read_input(file_path):
 
 def parse_line(line):
     symbols, numbers = line.split(' ', 1)
-    symbols = list(symbols.strip())
+    # symbols = list(symbols.strip())
     numbers = list(map(int, numbers.split(',')))
     return symbols, numbers
 
@@ -27,53 +27,96 @@ lines = read_input(file_path)
 springs, defects = process_input(lines)
 
 for idx, row in enumerate(springs):
-    row.append('?')
+    row += '?'
     springs[idx] = row*5
-    springs[idx].pop()
+    springs[idx] = springs[idx][:-1]
 
 for idx, row in enumerate(defects):
     defects[idx] = row*5
 
-def checkMatches(row, defects):
-    count = 0
-    modDefects = defects.copy()
-    for charIdx, char in enumerate(row):
-        if(char == '#'):
-            count += 1 
-        elif(count > 0 and count != modDefects[0]):
-            count = 0
-            return False
-        elif(count > 0):
-            count = 0
-            modDefects.pop(0)
-    if(count > 0 and count != modDefects[0]):
-        return False
-    return True
+
+@functools.cache
+def calc(record, groups):
+
+    # Did we run out of groups? We might still be valid
+    if not groups:
+
+        # Make sure there aren't any more damaged springs, if so, we're valid
+        if "#" not in record:
+            # This will return true even if record is empty, which is valid
+            return 1
+        else:
+            # More damaged springs that we can't fit
+            return 0
+
+    # There are more groups, but no more record
+    if not record:
+        return 0
+
+    # Look at the next element in each record and group
+    next_character = record[0]
+    next_group = groups[0]
+
+    # Logic that treats the first character as hashtag
+    def hashtag():
+
+        # If the first is a hashtag, then the first n characters must be
+        # able to be treated as a hashtag, where n is the first group number
+        this_group = record[:next_group]
+        this_group = this_group.replace("?", "#")
+
+        # If the next group can't fit all the damaged springs, then abort
+        if this_group != next_group * "#":
+            return 0
+
+        # If the rest of the record is just the last group, then we're
+        # done and there's only one possibility
+        if len(record) == next_group:
+            # Make sure this is the last group
+            if len(groups) == 1:
+                # We are valid
+                return 1
+            else:
+                # There's more groups, we can't make it work
+                return 0
+
+        # Make sure the character that follows this group can be a seperator
+        if record[next_group] in "?.":
+            # It can be seperator, so skip it and reduce to the next group
+            return calc(record[next_group+1:], groups[1:])
+
+        # Can't be handled, there are no possibilites
+        return 0
+
+    # Logic that treats the first character as a dot
+    def dot():
+        # We just skip over the dot looking for the next hashtag
+        return calc(record[1:], groups)
+
+    if next_character == '#':
+        # Test hashtag logic
+        out = hashtag()
+
+    elif next_character == '.':
+        # Test dot logic
+        out = dot()
+
+    elif next_character == '?':
+        # This character could be either character, so we'll explore both
+        # possibilities
+        out = dot() + hashtag()
+
+    else:
+        raise RuntimeError
+
+    # print(record, groups, out)
+    return out
+
 
 totalSumOfArrangements = 0
+
 for i, row in enumerate(springs):
-    questionMarks = []
-    broken = 0
-    totalBroken = 0
-    arrangements = 0
-    for defect in defects[i]:
-        totalBroken += defect
-    for j, symbol in enumerate(row):
-        if (symbol == '?'):
-            questionMarks.append([j])
-        elif symbol == '#':
-            broken += 1
-    brokenToAdd = totalBroken - broken
-    for variation in combinations(questionMarks, brokenToAdd):
-        newRow = row.copy()
-        for index in variation:
-            # print(index[0])
-            newRow[index[0]] = '#'
-        newRow = ['.' if element == '?' else element for element in newRow]
+    totalSumOfArrangements += calc(row, tuple(defects[i]))
 
-        if(checkMatches(newRow, defects[i])):
-            arrangements += 1
-    totalSumOfArrangements+=arrangements
-
-print(f"Part1: {totalSumOfArrangements}")
+print(f"Part2: {totalSumOfArrangements}")
 
